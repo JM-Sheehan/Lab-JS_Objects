697a023188bd4bc0b58c276cee6e779b0ac34a90
Jamie Sheehan <jamie.michael.sheehan@gmail.com>
2021-07-30 21:08:11 +0100

    Dynamic Properties

--- fundamentals.js
+++ fundamentals.js
@@ -12,7 +12,19 @@ const aCar = {
     },
 };
 
+aCar.mileage = 10000;
+aCar.color = {
+    exteriorColor: 'red',
+    interiorFabric: {
+        texture : 'leather',
+        shade: 'cream'
+    }
+};
+
 console.log(aCar.owner);
 console.log(aCar.owner + ' drives a ' + aCar.type.make)
 console.log('Reg. = ' + aCar.registration.year + '-'+
 aCar.registration.countyCode + '-' + aCar.registration.number + '.')
+console.log('It is a ' + aCar.color.exteriorColor + ' car, ' +
+aCar.mileage + ' mileage, with ' + 
+aCar.color.interiorFabric.texture + ' interior...');
