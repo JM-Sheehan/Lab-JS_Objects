8ec4e4ee66a309d35b21c00e9890136e4ef95137
Jamie Sheehan <jamie.michael.sheehan@gmail.com>
2021-08-24 12:52:03 +0100

    Final Commit

--- fundamentals.js
+++ fundamentals.js
@@ -1,5 +1,6 @@
 const aCar = {
     owner : "Joe Bloggs",
+    address : '3 Walkers Lane',
     previous_owners: [
         {
             name: 'Pat Smith',
@@ -20,6 +21,7 @@ const aCar = {
         county_code: 'WD',
         number: 1058
     },
+    features : ['Parking assist', 'Alarm', 'Tow-bar'],
 };
 
 aCar.mileage = 10000;
@@ -39,4 +41,16 @@ console.log('It is a ' + aCar.color.exterior_color + ' car, ' +
 aCar.mileage + ' mileage, with ' + 
 aCar.color.interior_fabric.texture + ' interior...');
 console.log('First owner : ' + aCar.previous_owners[0].name +  
-' - ' + aCar.previous_owners[0].address)
+' - ' + aCar.previous_owners[0].address);
+
+for (let i = 0 ; i < aCar.features.length ; i += 1) {
+    console.log(aCar.features[i]) ;
+} 
+
+for(let j = 0; j< aCar.previous_owners.length; j++){
+    console.log(aCar.previous_owners[j].name);
+}
+
+for (let p in aCar.type)  {
+    console.log(p.toUpperCase() + ' = ' + aCar.type[p] ) ;
+}
